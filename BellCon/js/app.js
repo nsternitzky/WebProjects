6c2fd503cf7675a64cbe9d8585a184f20ead5fac
Nick Sternitzky <nick.sternitzky@outlook.com>
2020-04-08 13:33:13 -0500

    Added js to expand schedule items when highlight links to them are clicked

--- BellCon/js/app.js
+++ BellCon/js/app.js
@@ -1,8 +1,20 @@
 const navbarList = document.querySelector('.navbar-nav');
 const navbarMenu = document.querySelector('#navbarNavAltMarkup');
+const highlightsList = document.querySelector('#highlights-list');
 
 navbarList.addEventListener('click', () => {
     if (navbarMenu.classList.contains('show')) {
         navbarMenu.classList.toggle('show');
+    }
+});
+
+highlightsList.addEventListener('click', (e) => {
+    const talkId = e.target.getAttribute('href');
+    const speaker = talkId.split('-')[0];
+    const talkDescriptionId = `${speaker}-desc`;
+    const talkDescription = document.querySelector(talkDescriptionId);
+    console.log(talkDescription);
+    if (talkDescription && !talkDescription.classList.contains('show')) {
+        talkDescription.classList.toggle('show');
     }
 });
