4675851cef6a1a5e495633d65f2f8f2b95aa8f32
Nick Sternitzky <nick.sternitzky@outlook.com>
2020-05-05 11:34:20 -0500

    Added comments

--- PokeLocator/js/app.js
+++ PokeLocator/js/app.js
@@ -2,6 +2,11 @@
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
+/**
+ * Fetches data and converts response to JSON
+ * @param {string} url - URL to fetch data from
+ * @return {Promise} Promise object representing fetched data 
+ */
 function fetchData(url) {
     return fetch(url)
              .then(checkStatus)  
@@ -9,6 +14,11 @@ function fetchData(url) {
              .catch(error => console.log('Error:', error))
 }
 
+/**
+ * Checks status of fetch call response
+ * @param {Response} Response object for fetch call
+ * @return {Promise} Promise object - resolved if response ok, rejected if not
+ */
 function checkStatus(response) {
     if (response.ok) {
         return Promise.resolve(response);
@@ -33,42 +43,61 @@ new Vue({
         locationList: []
     },
     methods: {
+        /**
+         * Gets called when user selects option from dropdown list
+         */
         handleSelect: function() {
             Promise.all([
-                fetchData(`http://pokeapi.co/api/v2/pokemon/${this.species.name}/encounters`),
-                fetchData(`http://pokeapi.co/api/v2/pokemon/${this.species.name}/`)
+                fetchData(`http://pokeapi.co/api/v2/pokemon/${this.species.name}/encounters`), //fetch location info
+                fetchData(`http://pokeapi.co/api/v2/pokemon/${this.species.name}/`) //fetch species info for img
             ])
             .then(data => {
 
+                //construct locationList from fetched data
                 this.locationList = data[0].reduce(function(obj,location) {
                     for (let versionDetails of location.version_details) {
+
+                        //if game version does not yet exist in obj array, create it
                         if (!obj.find(element => element.version === versionDetails.version.name)) {
                             obj.push({
                                 version: versionDetails.version.name,
                                 locations: []
                             });
                         }
+
+                        //push location to array associated with the corresponding game version
                         obj.find(element => element.version === versionDetails.version.name).locations.push(location.location_area.name);
                     }
                     return obj;
                 }, []);
 
+                //sort locationList by game version in order of release date using versionList
                 this.locationList.sort((a,b) => this.versionList.findIndex(item => item.name === a.version) - this.versionList.findIndex(item => item.name === b.version));
 
+                //set img to chosen species
                 this.species.img = data[1].sprites.front_default;
             })
         },
+        /**
+         * Format species name for display purposes
+         * @param {string} name - species name in raw data form
+         * @return {string} formatted - formatted species name
+         */
         formatSpeciesName: function(name) {
+            //capitalize name
             let formatted = this.capitalize(name);
 
+            //find hyphen at end of actual species name - exclude exceptional cases
             const hyphenRegEx = /-(?!(o(h*$|-))|z$|jr|mime)/g;
             const hyphenIndex = formatted.search(hyphenRegEx);
 
+            //change unnecessary hyphens to spaces and place parentheses around additional information
             if (hyphenIndex > -1) {
                 formatted = formatted.replace(hyphenRegEx,' ');
                 formatted = `${formatted.slice(0, hyphenIndex)} (${formatted.slice(hyphenIndex + 1)})`;
             }
 
+            //special cases
             if (name === 'mr-mime') {
                 formatted = 'Mr. Mime';
             }
@@ -79,20 +108,37 @@ new Vue({
 
             return formatted;
         },
+        /**
+         * Format location name for display purposes
+         * @param {string} location - location name in raw data form
+         * @return {string} formatted - formatted location name
+         */
         formatLocation: function(location) {
+            //capitalize location
             let formatted = this.capitalize(location);
+
+            //replace all hyphens with spaces
             formatted = formatted.replace(/-/g, ' ');
+
             return formatted;
         },
+        /**
+         * Format version name for display purposes
+         * @param {string} version - version name in raw data form
+         * @return {string} formatted - formatted version name
+         */
         formatVersion: function(version) {
+            //capitalize version
             let formatted = this.capitalize(version);
 
+            //replace hyphens with spaces and capitalize additional words in version title
             const hyphenIndex = formatted.search('-');
             if (hyphenIndex > -1) {
                 formatted = `${formatted.slice(0, hyphenIndex)} ${formatted.charAt(hyphenIndex + 1).toUpperCase()}${formatted.slice(hyphenIndex + 2)}`;
                 formatted = formatted.replace(/-/g, ' ');
             }
 
+            //special cases
             if (version === 'firered') {
                 formatted = 'FireRed';
             }
@@ -111,10 +157,18 @@ new Vue({
 
             return formatted;
         },
+        /**
+         * Capitalize first letter of string
+         * @param {string} text - string to be capitalized
+         * @return {string} capitalized string
+         */
         capitalize: function(text) {
             return text.charAt(0).toUpperCase() + text.slice(1);
         }
     },
+    /**
+     * Upon mounting, fetch data for species list and version list
+     */
     mounted() {
         fetchData('http://pokeapi.co/api/v2/pokemon/')
             .then(data => fetchData(`http://pokeapi.co/api/v2/pokemon/?limit=${data.count}`))
